3d167c371541ab41e37c83838b412ee18883febb
Steven Chun <steven.chun1208@gmail.com>
2016-12-25 22:53:44 -0800

    retry after 502 error

--- blackbelt/urls.py
+++ blackbelt/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^main', include('apps.lnr.urls')),
+    url(r'^', include('apps.lnr.urls')),
     url(r'^pokes', include('apps.blackbelt_app.urls')),
 ]
